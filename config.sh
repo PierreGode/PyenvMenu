30bd870146479d31ddb82d944a27af01a2787542
PierreGode <8579922+PierreGode@users.noreply.github.com>
2024-03-25 15:58:29 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-if ! cat "$HOME/.bashrc" | grep -q "^PYENV"
-then 
-echo "No config.. applying"
-echo 'export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi' >> $HOME/.bashrc
-sudo apt-get update && sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
-curl https://pyenv.run | bash
-else
-echo "config there"
+# Ensures the script is run as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
 fi
+ORIGINAL_USER=$(logname)
+USER_HOME=$(eval echo ~$ORIGINAL_USER)
+if ! grep -q "^PYENV" "$USER_HOME/.bashrc"; then
+  sudo -u $ORIGINAL_USER bash -c "echo 'export PYENV_ROOT=\"\$HOME/.pyenv\"
+export PATH=\"\$PYENV_ROOT/bin:\$PATH\"
+if command -v pyenv 1>/dev/null 2>&1; then
+   eval \"\$(pyenv init -)\"
+fi' >> $USER_HOME/.bashrc"
+fi
+sudo apt-get update && sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+sudo -u $ORIGINAL_USER curl https://pyenv.run | bash
