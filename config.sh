fc4f05cde44666a5a0b32bbd392f0fe9cb09c917
PierreGode <8579922+PierreGode@users.noreply.github.com>
2024-03-25 14:15:37 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 if ! cat $HOME/.bashrc | grep PYENV | head -1
-then echo 'export PYENV_ROOT="$HOME/.pyenv"
+then 
+echo 'export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
