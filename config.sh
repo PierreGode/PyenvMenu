4691d40929e6f0ab85a40b2091a68ad42e56cfe2
PierreGode <8579922+PierreGode@users.noreply.github.com>
2024-03-25 14:17:17 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 if ! cat $HOME/.bashrc | grep PYENV | head -1
 then 
+echo "No config.. applying"
 echo 'export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
@@ -7,4 +8,6 @@ if command -v pyenv 1>/dev/null 2>&1; then
 fi' >> $HOME/.bashrc
 sudo apt-get update && sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 curl https://pyenv.run | bash
+else
+echo "config there
 fi
