52c6b7ab7c85f2962a75ea798bdab8cf456585af
PierreGode <pierre@gode.one>
2024-06-04 14:54:50 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -15,3 +15,4 @@ fi' >> $USER_HOME/.bashrc"
 fi
 sudo apt-get update && sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 sudo -u $ORIGINAL_USER curl https://pyenv.run | bash
+sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.old
